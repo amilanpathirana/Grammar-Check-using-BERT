e2bc44a267bb6085dfb32eba13599be14cf66bb1
Amila Pathirana <emalpathirana@gmail.com>
2020-06-04 22:33:32 -0500

    config.py edit finished

--- src/config.py
+++ src/config.py
@@ -1,16 +1,15 @@
 import transformers
 
-
 DATA_PATH="./input/cola.tsv"
 BERT_PATH="./input/bert_base_uncased/"
 MODEL_PATH="model.bin"
-MAX_LEN=512
-TOKENIZER=transformers.BertTokenizer.from_pretrained(
-    BERT_PATH,
-    do_lower_case=True
-)
-
 
 TRAIN_BATCH_SIZE=1
 VALID_BATCH_SIZE=1
 EPOCHS=4
+
+TOKENIZER=transformers.BertTokenizer.from_pretrained(]
+    BERT_PATH,
+    do_lower_case=True
+)
+
